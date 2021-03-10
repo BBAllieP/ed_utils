a0cddbff98966c924d30034293ed23f1c4cdfc09
brabrah <brabrah@protonmail.com>
2021-03-10 08:45:02 +0100

    fixed icon for route planner

--- frontend/src/Pages/MenuItems.js
+++ frontend/src/Pages/MenuItems.js
@@ -3,11 +3,12 @@ import SecurityIcon from '@material-ui/icons/Security';
 import Shields from './Shields';
 import Home from './Home';
 import RoutePlanner from './RoutePlanner';
+import MapIcon from '@material-ui/icons/Map';
 
 const MenuItems = [
     {text: 'Home', icon: <HomeIcon />, route: '/', page: <Home />, isExact: true }, 
     {text: 'Shield Calculator', icon: <SecurityIcon />, route: '/shields', page: <Shields />},
-    {text: 'RoutePlanner', icon: <SecurityIcon />, route: '/route-planner', page: <RoutePlanner />},
+    {text: 'RoutePlanner', icon: <MapIcon />, route: '/route-planner', page: <RoutePlanner />},
 ];
 
 export default MenuItems;
