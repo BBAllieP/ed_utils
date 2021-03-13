fed94ef32e7ab0e87cb47962f9516fda72b7c2c6
brabrah <brabrah@protonmail.com>
2021-03-13 17:55:50 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Containers/App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#4f4f4f',
-    },
-    secondary: {
-      main: '#919191',
-    },
-  },
+	palette: {
+		primary: {
+			main: "#4f4f4f",
+		},
+		secondary: {
+			main: "#919191",
+		},
+	},
 });
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </Provider>,
-  rootElement
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</Provider>,
+	rootElement
 );
-
-
